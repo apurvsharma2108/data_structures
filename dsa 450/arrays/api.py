9b754492f92b942079c73e9944c2b4ca83c70bff
Apurv <apuravsharma244@gmail.com>
2022-09-15 23:28:55 +0530

    15 sep code

--- dsa 450/arrays/api.py
+++ dsa 450/arrays/api.py
@@ -1,14 +1,9 @@
-# Python program to print all
-# sublist from a given list
+def check(a):
+	a[0]=100
+	for i in a:
+		print(i)
+a= [1,2,3]
 
-# function to generate all the sub lists
-def sub_lists (l):
-	lists = [[]]
-	for i in range(len(l) + 1):
-		for j in range(i):
-			lists.append(l[j: i])
-	return lists
-
-# driver code
-l1 = [64, -15, 54, -79, 16]
-print(sub_lists(l1))
+print(check(a))
+for i in a:
+	print(i)
